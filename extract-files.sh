d6990f9df788a2fed03a7021e213388369aa277e
Vasyl Gello <vasek.gello@gmail.com>
2018-08-30 23:38:31 +0300

    ha3g: Import common vendor scripts
    Import common extract-files.sh and setup-makefiles.sh
    from klte.
    
    Also add functionality to wait for "adb root",
    to ensure extraction of system/bin/gpsd.
    
    Change-Id: I635dbb662bb79d7e66c78049f5880b134a11d763

--- extract-files.sh
+++ extract-files.sh
@@ -1,56 +1,37 @@
 #!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 set -e
-export DEVICE=ha3g
-export VENDOR=samsung
 
-if [ $# -eq 0 ]; then
-  SRC=adb
-else
-  if [ $# -eq 1 ]; then
-    SRC=$1
-  else
-    echo "$0: bad number of arguments"
-    echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM]"
-    echo ""
-    echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
-    echo "the device using adb pull."
-    exit 1
-  fi
+export DEVICE=ha3g
+export DEVICE_COMMON=exynos5420-common
+export VENDOR=samsung
+export INITIAL_COPYRIGHT_YEAR=2016
+
+if [ -z "$STOCK_ROM_DIR" ]; then
+    if [ "$(adb root | grep -c 'adbd')" -eq 1  ]; then
+        echo "adbd root: OK!"
+        adb wait-for-device
+    else
+        echo "/system/bin/gpsd requires root privileges to be extracted"
+        echo "root access is disabled by system setting - enable in settings -> development options"
+        adb wait-for-device
+        exit
+    fi
 fi
 
-BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
-rm -rf $BASE/*
-
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-  echo "Extracting /system/$FILE ..."
-  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-  FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-  DEST=${PARSING_ARRAY[1]}
-  if [ -z $DEST ]
-  then
-    DEST=$FILE
-  fi
-  DIR=`dirname $FILE`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$FILE $BASE/$DEST
-  # if file dot not exist try destination
-    if [ "$?" != "0" ]
-        then
-        adb pull /system/$DEST $BASE/$DEST
-    fi
-  else
-    cp $SRC/system/$FILE $BASE/$DEST
-    # if file dot not exist try destination
-    if [ "$?" != "0" ]
-        then
-        cp $SRC/system/$DEST $BASE/$DEST
-    fi
-  fi
-done
-
-./setup-makefiles.sh
+./../$DEVICE_COMMON/extract-files.sh $@
